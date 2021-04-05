68375d3f5a302c6797406aad7c42555b7cecc1c0
Irene <irene1martin2menendez@gmail.com>
2021-04-05 17:11:38 +0100

    Updated to be responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,21 @@ import {Navbar, Form, FormControl, Button, Nav} from "react-bootstrap";
 export function Header() {
     return (
         <div className="header-area">
-            <Navbar bg="light" variant="light">
+            <Navbar bg="light" expand="lg" variant="light">
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+
                 <Navbar.Brand className="logoName" href="/">La Ultima página</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="/article">Artículos</Nav.Link>
-                    <Nav.Link href="/About">Sobre mi</Nav.Link>
-                    <Nav.Link href="" title="Available soon">Tienda</Nav.Link>
-                </Nav>
-                <Form inline className="search">
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                    <Button variant="outline-primary">Search</Button>
-                </Form>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/article">Artículos</Nav.Link>
+                        <Nav.Link href="/About">Sobre mi</Nav.Link>
+                        <Nav.Link href="" title="Available soon">Tienda</Nav.Link>
+                    </Nav>
+                    <Form inline className="search">
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                        <Button variant="outline-primary">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
             </Navbar>
         </div>
     );
